82f8819763cf583b0eed0d18f295bb48eefcb915
DESKTOP-U6I7H4K\class01 <wooseong1001@naver.com>
2023-09-07 17:56:47 +0900

    0907insert

--- src/main/java/com/example/project_sample/dao/day/DayDao.java
+++ src/main/java/com/example/project_sample/dao/day/DayDao.java
@@ -1,7 +1,5 @@
 package com.example.project_sample.dao.day;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.project_sample.vo.day.DayVo;
@@ -10,17 +8,9 @@ import com.example.project_sample.vo.day.DayVo;
 @Mapper
 public interface DayDao {
     
-    List<DayVo> selectList();//전체조회
+    int dayinsert(DayVo vo);
 
-    
-    int insert(DayVo vo);
-
-    int update(DayVo vo);
-
-    int delete(int d_idx);
-
-
-    DayVo dayPlus(DayVo vo);
+    DayVo recentData();
 
 
 
